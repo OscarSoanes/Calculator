fd68faf181b8247642a786930aa29605c487ea4d
Oscar Soanes <oscarsoanes@gmail.com>
2023-01-22 13:24:22 +0000

    core functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add (previous, current) {
+    return previous + current;
+}
+
+function subtract (previous, current) {
+    return previous - current;
+}
+
+function multiply (previous, current) {
+    return previous * current;
+}
+
+function divide (previous, current) {
+    return previous / current;
+}
